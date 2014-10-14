8a30e43b713abc134a5ebedb3a4b4ce9f1bdbd15
Peter Hellberg <peter@c7.se>
2014-10-14 22:05:00 +0200

    Added missing comment

--- voice.go
+++ voice.go
@@ -5,6 +5,8 @@ import (
 	"os/exec"
 )
 
+// Say executes the say command with the
+// provided arguments as the string being said
 func Say(d ...interface{}) {
 	exec.Command("say", fmt.Sprintf("%v", d), "-v", "Vicki").Run()
 }
