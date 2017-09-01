82f98ba629330ddc64f4cd48cc2a9de23f57a855
Peter Hellberg <peter@c7.se>
2017-09-02 00:32:28 +0200

    The voice of Vicki were removed from macOS
    (Making it configurable using voice.Name = "Name")
    
    Default voice set to Samantha

--- voice.go
+++ voice.go
@@ -5,8 +5,12 @@ import (
 	"os/exec"
 )
 
+// Name of the voice use by the Say function.
+// List all voices by running: say -v ?
+var Name = "Samantha"
+
 // Say executes the say command with the
 // provided arguments as the string being said
 func Say(d ...interface{}) {
-	exec.Command("say", fmt.Sprintf("%v", d), "-v", "Vicki").Run()
+	exec.Command("say", fmt.Sprintf("%v", d), "-v", Name).Run()
 }
